383a0a26fe279efe9dcf145549a026c04b7406d4
twwright <twwright@gmail.com>
2020-02-23 21:49:02 -0600

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,16 @@
 class Pirate
+	attr_accessor :name, :weight, :height
+
+	@@all = []
+
+	def initialize(info)
+		@name = info[:name]
+		@weight = info[:weight]
+		@height = info[:height]
+		@@all << self
+	end
+
+	def self.all
+		@@all
+	end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,20 @@
 class Ship
+	attr_accessor :name, :type, :booty
+
+	@@all = []
+
+	def initialize(info)
+		@name = info[:name]
+		@type = info[:type]
+		@booty = info[:booty]
+		@@all << self
+	end
+
+	def self.all
+		@@all
+	end
+
+	def self.clear
+		@@all = []
+	end
 end
