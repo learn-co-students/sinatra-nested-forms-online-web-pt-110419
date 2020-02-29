fb9ac3a3413182664f599173347d466845fd36be
Alex Lee <hyunho98@gmail.com>
2020-02-29 04:07:08 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,17 @@
 class Pirate
+  attr_accessor :name, :weight, :height, :ships
+
+  @@all = []
+
+  def initialize(attributes)
+    @name = attributes[:name]
+    @weight = attributes[:weight]
+    @height = attributes[:height]
+    @ships = attributes[:ships]
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
