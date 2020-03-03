a401bd992a1fc99e41bb74674edc0fad948f0841
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-03-03 23:39:01 +0000

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,20 @@
 class Ship
+  
+  attraccessor = :name, :type, :booty
+  @@all = []
+  
+  def self.all
+    @@all
+  end
+  
+  def self.clear
+    @@all.clear
+  end
+  
+  def initialize(name, type, booty)
+    @name = name
+    @type = type
+    @booty = booty
+    @@all << self
+  end
 end
